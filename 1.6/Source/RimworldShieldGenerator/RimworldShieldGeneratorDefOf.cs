e1ef91fb9570cd701d1bae1256b5c637e2ffebb3
John Cannon <john.arc.cannon@gmail.com>
2025-10-26 21:41:20 +0000

    Fix incorrect mote being stored for no reason, fix generator actually generating power in the default XML instead of using it...

--- 1.6/Source/RimworldShieldGenerator/RimworldShieldGeneratorDefOf.cs
+++ 1.6/Source/RimworldShieldGenerator/RimworldShieldGeneratorDefOf.cs
@@ -6,7 +6,6 @@ namespace RimworldShieldGenerator
     [DefOf]
     public static class RimworldShieldGeneratorDefOf
     {
-        public static ThingDef Mote_LaserGraserBeamBase;
         public static SoundDef Laser_Fire;
 
         static RimworldShieldGeneratorDefOf()
